7909be5f2039e48672fad2d7460162b42a3f98e6
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-10 12:50:03 -0300

    Create Exe13_Dias_da_semana.py

---
+++ Exe13_Dias_da_semana.py
@@ -0,0 +1,18 @@
+print("Digite um numero inteiro, de 1 a 7 que retornarei qual dia da semana é")
+dia = int(input())
+if dia == 1:
+    print("Domingo")
+elif dia == 2:
+    print("Segunda")
+elif  dia == 3:
+    print("Terça")
+elif dia == 4:
+    print("Quarta")
+elif dia == 5:
+    print("Quinta")
+elif dia == 6:
+    print("Sexta")
+elif dia == 7:
+    print("Sabado")
+else:
+    print("Dia invalido")
