09b4923944f8b3cca200b72beca9f24d4b1859a6
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-10 06:58:21 -0300

    Create Exe4_vogal_ou_consoante.py

---
+++ Exe4_vogal_ou_consoante.py
@@ -0,0 +1,5 @@
+letra = input("Digite uma letra: ").lower()
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u" or letra == "ão":
+    print("A letra digitada é uma vogal")
+else:
+    print("A letra digitada é uma consoante")
