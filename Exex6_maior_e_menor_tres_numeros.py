6abd1d52de25d3729a02a0efcc225e8087becf63
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-10 07:40:23 -0300

    Create Exex6_maior_e_menor_tres_numeros.py

---
+++ Exex6_maior_e_menor_tres_numeros.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite um numero; "))
+num2 = float(input("Digite um numero; "))
+num3 = float(input("Digite um numero; "))
+maior = max(num1,num2,num3)
+menor = min(num1,num2,num3)
+print(f"O maior valor é {maior}")
+print(f"O maior valor é {menor}")
+
