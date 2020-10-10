aff6707113d9e75ce196154a1ccae0ededaf47f0
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-10 07:32:42 -0300

    Update Exe6_maior_entre_tres_numeros.py

--- Exe6_maior_entre_tres_numeros.py
+++ Exe6_maior_entre_tres_numeros.py
@@ -1,11 +1,11 @@
-"""
+
 num1 = float(input("Digite um numero; "))
 num2 = float(input("Digite um numero; "))
 num3 = float(input("Digite um numero; "))
 maior = max(num1,num2,num3)
 print(f"O maior valor é {maior}")
-"""
 
+"""
 #Outra solução possivel
 
 num1 = float(input("Digite um numero; "))
@@ -18,4 +18,4 @@ if num2 > num1 and num2 > num3:
 if num3 > num1 and num3 > num2:
     maior = num3
     print(f"O maior numero é {maior}")
-
+"""
