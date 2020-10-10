9a53de4638557350b66caef9480f7109fbf48009
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-10 06:54:48 -0300

    Create Exe3_Masculino_ou_Feminino.py

---
+++ Exe3_Masculino_ou_Feminino.py
@@ -0,0 +1,7 @@
+sexo = input("Digite F para feminino ou M para masculino: ").upper()
+if sexo == "F":
+    print("Sexo feminuno")
+elif sexo == "M":
+    print("Sexo masculino")
+else:
+    print("Entrada invalida")
