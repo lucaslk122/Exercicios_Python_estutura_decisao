ccc433635345618577c1e1041802b9155ce1e4bd
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-10 06:50:51 -0300

    Create Exe2_numero_negativo_ou_positivo.py

---
+++ Exe2_numero_negativo_ou_positivo.py
@@ -0,0 +1,7 @@
+numero = float(input("Entre com um numero: "))
+if numero < 0:
+    print("Numero negativo")
+elif  numero > 0:
+    print("Numero positivo")
+else:
+    print("0 é considerado um numero neutro")
