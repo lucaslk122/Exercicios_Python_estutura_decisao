c293391188a6f94788958d973d17c8cdf420a031
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-13 13:06:34 -0300

    Update Exe27_Fruteira_calculo.py

--- Exe27_Fruteira_calculo.py
+++ Exe27_Fruteira_calculo.py
@@ -3,4 +3,5 @@ print("Disponibilizamos de maças e morangos,de acordo com a tabela a seguir")
 print(""" Morango até 5Kg, R$2.50/Kg, acima é R$2.20/kg
  Maças até 5Kg, R$1,80/Kg, acima é R$1.50/Kg""")
 print("Tambem damos desconto de 10% se comprar mais de 8Kg em fruta ou o valor total ultrapassar R$25.00")
-
+morango_kilos = float(input("Digite quantos kilo de morango voce vai comprar: "))
+maças_kilos = float(input("Digite quantos kilo de mmaça voce vai comprar: "))
