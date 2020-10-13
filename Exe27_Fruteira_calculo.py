ea9fe53641a9f59dffa4347e756657029e239b5d
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-13 12:13:47 -0300

    Create Exe27_Fruteira_calculo.py

---
+++ Exe27_Fruteira_calculo.py
@@ -0,0 +1,6 @@
+print("Frutaria Samofalov, bem vindo")
+print("Disponibilizamos de maças e morangos,de acordo com a tabela a seguir")
+print(""" Morango até 5Kg, R$2.50/Kg, acima é R$2.20/kg
+ Maças até 5Kg, R$1,80/Kg, acima é R$1.50/Kg""")
+print("Tambem damos desconto de 10% se comprar mais de 8Kg em fruta ou o valor total ultrapassar R$25.00")
+
