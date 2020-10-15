4373cb788e43d214a926e86202d5228e03e7f395
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-15 08:29:46 -0300

    Update Exe19_centena_dezena_unidade.py

--- Exe19_centena_dezena_unidade.py
+++ Exe19_centena_dezena_unidade.py
@@ -3,7 +3,9 @@ if numero > 1000:
     print("Numero invalido")
 else:
     unidade = numero % 10
-    dezena = int(((numero - unidade) / 10) % 10)
-    centena = int((numero - dezena) / 10)
+    numero = (numero - unidade) / 10
+    dezena = int(numero % 10)
+    numero = (numero - dezena) / 10
+    centena = int(numero)
     print(f"{centena} centena(s) , {dezena} dezena(s) e {unidade} unidade(s)")
 
