51a9699c5d85aa6cfb163d891c56e804a7255634
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-15 08:52:51 -0300

    Update Exe24_operação_numeros.py

--- Exe24_operação_numeros.py
+++ Exe24_operação_numeros.py
@@ -12,57 +12,57 @@ if opção == 1:
     soma = numero1 + numero2
     if soma > 0 :
         print("Numero positivo")
+    else:
+        print("Numero negativo")
     if soma % 2 == 0:
         print("Numero par")
+    else:
+        print("Numero impar")
     if soma == round(soma):
         print("Numero inteiro")
-    if soma < 0:
-        print("Numero negativo")
-    if soma % 2 != 0:
-        print("Numero impar")
-    if soma != round(soma):
+    else:
         print("Numero decimal")
 elif opção == 2:
     subtração = numero1 - numero2
     if subtração > 0 :
         print("Numero positivo")
+    else:
+        print("Numero negativo")
     if subtração % 2 == 0:
         print("Numero par")
+    else:
+        print("Numero impar")
     if subtração == round(subtração):
         print("Numero inteiro")
-    if subtração < 0:
-        print("Numero negativo")
-    if subtração % 2 != 0:
-        print("Numero impar")
-    if subtração != round(subtração):
+    else:
         print("Numero decimal")
 elif opção == 3:
     divisão = numero1 / numero2
     if divisão > 0 :
         print("Numero positivo")
+    else:
+        print("Numero negativo")
     if divisão % 2 == 0:
         print("Numero par")
+    else:
+        print("Numero impar")
     if divisão == round(divisão):
         print("Numero inteiro")
-    if divisão < 0:
-        print("Numero negativo")
-    if divisão % 2 != 0:
-        print("Numero impar")
-    if divisão != round(divisão):
+    else:
         print("Numero decimal")
 elif opção == 4:
     multiplicação = numero1 * numero2
     if multiplicação > 0 :
         print("Numero positivo")
+    else:
+        print("Numero negativo")
     if multiplicação % 2 == 0:
         print("Numero par")
+    else:
+        print("Numero impar")
     if multiplicação == round(multiplicação):
         print("Numero inteiro")
-    if multiplicação < 0:
-        print("Numero negativo")
-    if multiplicação % 2 != 0:
-        print("Numero impar")
-    if multiplicação != round(multiplicação):
+    else:
         print("Numero decimal")
 else:
     print("Opção invalida, reinicie o programa e forneça uma opção válida")
